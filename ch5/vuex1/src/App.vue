363487bef55f026c78b154c521d6bbcb383fc332
lukeexer <lientuna@gmail.com>
2021-12-30 23:54:58 +0800

    accomplish ch5 vuex exercises

--- ch5/vuex1/src/App.vue
+++ ch5/vuex1/src/App.vue
@@ -1,15 +1,40 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <img alt="Vue logo" src="./assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>{{ greeting }}</h1>
+    <h1>{{ product }}</h1>
+    <h1>{{ price }}</h1>
+    <h1>{{ sellingPrice }}</h1>
+    <button @click="setQuantity(qty)">Click</button>
+    <div>{{ productInfo }}</div>
+  </div>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import { mapState, mapGetters, mapMutations, mapActions } from 'vuex'
 
 export default {
   name: 'App',
   components: {
     HelloWorld
+  },
+  data() {
+    return {
+      qty: 0
+    } 
+  },
+  computed: {
+    ...mapState(['greeting', 'product', 'price', 'quantity', 'productInfo']),
+    ...mapGetters(['sellingPrice'])
+  },
+  methods: {
+    ...mapMutations(['setQuantity']),
+    ...mapActions(['fetchProductInfo'])
+  },
+  created() {
+    this.fetchProductInfo({id: 1})
   }
 }
 </script>
--- ch5/vuex1/src/store/index.js
+++ ch5/vuex1/src/store/index.js
@@ -2,10 +2,34 @@ import { createStore } from 'vuex'
 
 export default createStore({
   state: {
+    greeting: 'Hello 008 Vue.js!',
+    product: '008-JS',
+    price: 500,
+    quantity: 100,
+    productInfo: ""
+  },
+  getters: {
+    discount(state){
+      return (state.quantity > 50) ? 0.8 : 0.9;
+    },
+    sellingPrice(state, getters) {
+      return state.price * getters.discount;
+    }
   },
   mutations: {
+    setQuantity(state, payload) {
+      state.quantity = payload.qty;
+    },
+    setProductInfo(state, payload) {
+      state.productInfo = payload;
+    }
   },
   actions: {
+    fetchProductInfo(context, payload) {
+      fetch('https://jsonplaceholder.typicode.com/users/' + payload.id)
+        .then(res => res.json())
+        .then(data => context.commit('setProductInfo', data));
+    }
   },
   modules: {
   }
